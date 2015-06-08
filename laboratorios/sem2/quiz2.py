ad7314e82baeb5b5518dd5ed388358ad94bb7a71
abdiel0995 <abdiel0995@hotmail.com>
2015-06-07 22:30:14 -0500

    Update quiz2.py

--- laboratorios/sem2/quiz2.py
+++ laboratorios/sem2/quiz2.py
@@ -2,5 +2,3 @@ Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (In
 Type "copyright", "credits" or "license()" for more information.
 >>> nombre = "Abdiel Castillo"
 >>> print ("Hola "+nombre)
-Hola Abdiel Castillo
->>> 
