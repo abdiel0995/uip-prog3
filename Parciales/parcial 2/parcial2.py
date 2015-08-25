5f51c907092e56752c871e4b5d983d816ae69e80
abdiel0995 <abdiel0995@hotmail.com>
2015-08-24 21:35:21 -0500

    Parcial 2 - Incompleto

---
+++ Parciales/parcial 2/parcial2.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.screenmanager import Screen, SlideTransition
+class TraductorApp(App):
+	pass
+if __name__ == '__main__':
+	TraductorApp().run()
+	
+dic = {'perro': "dog", 'gato': "cat", 'serpiente': "snake", "vaca":"cow", "caballo":"horse", "gallina":"chicken", "pez":"fish", "tortuga":"turtle", "tiburon":"shark", "cocodrilo":"crocodile", "mono":"monkey"}
